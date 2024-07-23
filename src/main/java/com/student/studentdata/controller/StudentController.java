a1a4305ae57d5e33f35266b9a935121707bf2dc5
Basavaraj <basuburli1998@gmail.com>
2024-07-23 15:14:49 +0530

    did some changes in controller and service

--- src/main/java/com/student/studentdata/controller/StudentController.java
+++ src/main/java/com/student/studentdata/controller/StudentController.java
@@ -1,12 +1,41 @@
 package com.student.studentdata.controller;
 
+import com.student.studentdata.entity.Student;
 import com.student.studentdata.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
+
+    @PostMapping("/saveStudent")
+    public Student addStudent(@RequestBody Student student){
+        return  studentService.saveStudent(student);
+    }
+
+    @PostMapping("/addStudents")
+    public List<Student> addStudents(@RequestBody List<Student> students){
+        return studentService.saveStudents(students);
+    }
+
+    @GetMapping("/getStudent/{id}")
+    public Student getStudent(@PathVariable int id){
+        return  studentService.getStudent(id);
+    }
+
+    @GetMapping("/getAllStudent")
+    public  List<Student>  getAllStudent(){
+         return studentService.getAllStudent();
+    }
+
+    @DeleteMapping("/deleteStudent/{id}")
+    public  String deleteStudentById(@PathVariable int id){
+        studentService.deleteStudentById(id);
+        return "Student deleted";
+    }
 }
--- src/main/java/com/student/studentdata/service/StudentService.java
+++ src/main/java/com/student/studentdata/service/StudentService.java
@@ -1,10 +1,39 @@
 package com.student.studentdata.service;
 
+import com.student.studentdata.entity.Student;
 import com.student.studentdata.repo.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+@Service
 public class StudentService {
 
     @Autowired
     private StudentRepo studentRepo;
+
+
+    public Student saveStudent(Student student){
+        return  studentRepo.save(student);
+    }
+
+    public List<Student> saveStudents(List<Student> students){
+        return  studentRepo.saveAll(students);
+    }
+
+    public Student getStudent(int id){
+        return studentRepo.findById(id).orElse(null);
+
+    }
+
+    public List<Student> getAllStudent(){
+       return   studentRepo.findAll();
+
+    }
+
+    public  String deleteStudentById(int id){
+        studentRepo.deleteById(id);
+        return "student deleted";
+    }
+
 }
